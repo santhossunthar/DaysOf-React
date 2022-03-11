44dffb97fb5f3b962f6be8c0c3e939174d2b6740
santhossunthar <santhoshsunthar@gmail.com>
2022-03-12 01:07:11 +0530

    Added DB data insertion

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const db = require('./db');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.post('/api/keys', (req, res) => {
     const keyData = req.body;
-    console.log(keyData.key)
-    res.send(true)
+    const keyId = keyData.keyId;
+    const keys = keyData.keys;
+
+    const sql = `INSERT INTO keystroke (id, keystring) VALUES ('${keyId}', ' ')`;
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            console.error('Error:', err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.json(results);
+        }
+    });
 });
 
 app.listen(port)
